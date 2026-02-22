f26ff6eb06007e458f85eef7b2871204975e14f9
amirkhezri <85443248+amirkhezri@users.noreply.github.com>
2026-02-22 19:21:07 +0330

    Update main.js

--- main.js
+++ main.js
@@ -6,7 +6,7 @@ let isProcessing = false; // Anti-spam lock
 const YOOMONEY_RECIPIENT_ID = '4100119271147598';
 const BOT_USERNAME = 'Toni_vpn_bot';
 const TRIAL_DAYS = 3;
-const API_BASE = "https://toni-vpn.onrender.com:10000/api"
+const API_BASE = "https://toni-vpn.onrender.com:5000/api"
 
 
 
